2b7603319de9ce2111433d923b2eea1a4a968b26
emmanuelmartser <81562609+emmanuelmartser@users.noreply.github.com>
2021-04-05 22:16:07 +0200

    v0 of Read_Holo.m

---
+++ Matlab/Read_Holo.m
@@ -0,0 +1,62 @@
+function Read_Holo()
+
+[filename,path] = uigetfile('*.holo');
+if isequal(filename,0)
+   disp('User selected Cancel');
+else
+   disp(['User selected ', fullfile(path, filename)]);
+
+header_mmap = memmapfile(filename, 'Format',...
+            {'uint8', 4, 'magic_number';...
+             'uint16', 1, 'version';...
+             'uint16', 1, 'bit_depth';...
+             'uint32', 1, 'width';...
+             'uint32', 1, 'height';...
+             'uint32', 1, 'num_frames';...
+             'uint64', 1, 'total_size';...
+             'uint8', 1,  'endianness';...
+             % padding - skip
+            }, 'Repeat', 1);
+        
+if ~isequal(header_mmap.Data.magic_number', unicode2native('HOLO'))
+    error('Bad holo file.');
+end
+
+%version = header_mmap.Data.version;
+num_frames = header_mmap.Data.num_frames;
+frame_width = header_mmap.Data.width;
+frame_height = header_mmap.Data.height;
+%data_size = header_mmap.Data.total_size;
+bit_depth = header_mmap.Data.bit_depth;
+endianness = header_mmap.Data.endianness;
+
+if endianness == 0
+    endian= 'l';
+else
+    endian= 'b';
+end
+
+fd = fopen(filename, 'r');
+offset = 65;
+frame_batch = zeros(frame_width, frame_height, num_frames, 'uint8'); % single 
+frame_size = frame_width * frame_height * uint32(bit_depth / 8);
+width_range = 1:frame_width;
+height_range = 1:frame_height; 
+fseek(fd, offset, 'bof');
+
+for i = 1:num_frames
+    fseek(fd, offset + frame_size * (i-1), 'bof'); 
+    if bit_depth == 8
+        frame_batch(width_range, height_range, i) = reshape(fread(fd, frame_width * frame_height, 'uint8=>uint8', endian), frame_width, frame_height);
+    else
+        frame_batch(width_range, height_range, i) = reshape(fread(fd, frame_width * frame_height, 'uint16=>single', endian), frame_width, frame_height);
+    end 
+   
+end
+
+implay(frame_batch(:,:,:),30); %30 fps 
+fclose(fd);
+
+end
+
+
