7c5d7021c3ad447a10430057f38f28d81fee8545
Thomas Jarrossay <jarrossay.thomas@gmail.com>
2021-04-15 17:05:24 +0200

    v1.0.3 ImageJ Plugin

--- ImageJ/Holo/About_.java
+++ ImageJ/Holo/About_.java
@@ -9,7 +9,7 @@ import ij.plugin.*;
 
 public class About_ implements PlugIn
 {
-    private String version = "1.0.2";
+    private String version = "1.0.3";
     private String holo_file_specification_link = "https://ftp.espci.fr/incoming/Atlan/holovibes/holo/HoloFileSpecification.pdf";
     private String github_repository = "https://github.com/holostuff/ImportExportScriptsForHoloFiles/";
     
