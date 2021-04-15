07b6a47c42ec493ec35cb43227977a8424f077cb
Thomas Jarrossay <jarrossay.thomas@gmail.com>
2021-04-15 11:26:43 +0200

    Adding github repository to About section

--- ImageJ/Holo/About_.java
+++ ImageJ/Holo/About_.java
@@ -11,6 +11,7 @@ public class About_ implements PlugIn
 {
     private String version = "1.0.0";
     private String holo_file_specification_link = "https://ftp.espci.fr/incoming/Atlan/holovibes/holo/HoloFileSpecification.pdf";
+    private String github_repository = "https://github.com/holostuff/ImportExportScriptsForHoloFiles/";
     
     public void run(String arg)
     {
@@ -19,6 +20,7 @@ public class About_ implements PlugIn
         + "<br/>"
         + "<center>This set of plugins aims to facilitate the opening and saving of holo files.</center>"
         + "<br/>"
+        + "<center>Checkout our <a href=" + github_repository + ">github</a> !</center>"
         + "<center>Find more at: " + "<a href=" + holo_file_specification_link + ">HoloFileSpecification</a></center>");
     }
 }
